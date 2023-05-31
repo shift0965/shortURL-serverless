f441b3840bb160d0a552c61b4cf637d857d8e5b5
ShaoTing Fong <fengshaoting@fengshaotingde-MacBook-Pro.local>
2023-05-31 18:42:59 +0800

    feature getURL done

---
+++ lambdas/getUrl.js
@@ -0,0 +1,35 @@
+const { DynamoDB } = require("aws-sdk");
+const dbClient = new DynamoDB.DocumentClient();
+
+exports.handler = async function (event) {
+  try {
+    const params = {
+      TableName: process.env.TABLE_NAME,
+      Key: {
+        shortURL: event.pathParameters.shortURL,
+      },
+    };
+    console.log(params);
+
+    const data = await dbClient.get(params).promise();
+    console.log(data);
+
+    if (data.Item) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ originalURL: data.Item.originalURL }),
+      };
+    } else {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ error: "Item not found" }),
+      };
+    }
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Error" }),
+    };
+  }
+};
